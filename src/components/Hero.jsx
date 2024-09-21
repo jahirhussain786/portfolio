0bc702c58c364f871e4281eb41eb1a824d5ee6e3
Jahir hussain <codewithjahir@gmail.com>
2024-09-21 19:04:30 +0530

    scroll button added for mobile screens

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { styles } from '../style';
 import { ComputersCanvas } from './canvas';
+import { motion } from 'framer-motion';
 
 const Hero = () => {
   return (
-    <section id='home' className='relative w-full h-screen mx-auto overflow-hidden'>
+    <section id='home' className='relative w-full h-screen mx-auto overflow-hidden cursor-default'>
       {/* Content Container */}
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
@@ -20,9 +21,27 @@ const Hero = () => {
           </p>
         </div>
       </div>
+     
       
       {/* Canvas Component */}
       <ComputersCanvas />
+      <div className='absolute bottom-10 lg:hidden w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[32px] h-[55px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
